1004a482a9b5bde14b90265071693f31f969e98f
epee <eroloo1995@gmail.com>
2023-01-08 15:31:39 +0100

    tests, migrations

--- app/app/tests.py
+++ app/app/tests.py
@@ -11,10 +11,10 @@ class CalcTests(SimpleTestCase):
 
     def test_add_number(self):
         """Test adding number together"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
     def test_subtract_number(self):
         """Test substracting numbers"""
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
         self.assertEqual(res, 5)
