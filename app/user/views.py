d25f5f6fb6f924212fe4643a0f8de567ce0cea1b
epee <eroloo1995@gmail.com>
2023-01-12 14:55:49 +0100

    poprawki

--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@
 Views for the user API.
 """
 from rest_framework import generics
-from user.serializes import UserSerializer
+from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
